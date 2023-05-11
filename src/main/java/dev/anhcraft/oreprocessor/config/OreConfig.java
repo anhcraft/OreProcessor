1da362acb06ce851e07d582dde0a8af6b915e26f
anhcraft <huynhduyanh123123@gmail.com>
2023-05-11 21:59:47 +0700

    Fix NPE on below 1.17

--- src/main/java/dev/anhcraft/oreprocessor/config/OreConfig.java
+++ src/main/java/dev/anhcraft/oreprocessor/config/OreConfig.java
@@ -1,13 +1,22 @@
 package dev.anhcraft.oreprocessor.config;
 
 import dev.anhcraft.config.annotations.Configurable;
+import dev.anhcraft.config.annotations.PostHandler;
 import org.bukkit.Material;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Configurable(keyNamingStyle = Configurable.NamingStyle.TRAIN_CASE)
 public class OreConfig {
     public String name;
     public List<Material> blocks;
     public List<Material> rawMaterials;
+
+    @PostHandler
+    private void postProcess() {
+        blocks = blocks.stream().filter(Objects::nonNull).collect(Collectors.toList());
+        rawMaterials = rawMaterials.stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
 }
