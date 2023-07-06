811ddfdff0b0f4d7b2a01aabb199e2d2780e3b45
anhcraft <huynhduyanh123123@gmail.com>
2023-07-06 17:23:24 +0700

    Fix NPE related to stats

--- plugin/src/main/java/dev/anhcraft/oreprocessor/storage/stats/StatisticHelper.java
+++ plugin/src/main/java/dev/anhcraft/oreprocessor/storage/stats/StatisticHelper.java
@@ -5,16 +5,16 @@ import dev.anhcraft.oreprocessor.api.data.stats.Monitored;
 public class StatisticHelper {
     public static void increaseMiningCount(String ore, Monitored trackedData) {
         trackedData.getCumulativeStats().addMiningCount(ore, 1);
-        trackedData.getHourlyStats(System.currentTimeMillis()).addMiningCount(ore, 1);
+        trackedData.getOrCreateHourlyStats(System.currentTimeMillis()).addMiningCount(ore, 1);
     }
 
     public static void increaseFeedstockCount(String ore, int amount, Monitored trackedData) {
         trackedData.getCumulativeStats().addFeedstockCount(ore, amount);
-        trackedData.getHourlyStats(System.currentTimeMillis()).addFeedstockCount(ore, amount);
+        trackedData.getOrCreateHourlyStats(System.currentTimeMillis()).addFeedstockCount(ore, amount);
     }
 
     public static void increaseProductCount(String ore, int amount, Monitored trackedData) {
         trackedData.getCumulativeStats().addProductCount(ore, amount);
-        trackedData.getHourlyStats(System.currentTimeMillis()).addProductCount(ore, amount);
+        trackedData.getOrCreateHourlyStats(System.currentTimeMillis()).addProductCount(ore, amount);
     }
 }
